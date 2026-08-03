d6ea4f380c723a399da3ca0a5f4dcebb78358b32
Binaro225 <131581665+Binaro225@users.noreply.github.com>
2026-08-03 12:16:23 +0000

    Initialize main.jsx with React rendering

---
+++ main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import JarvisInterface from './Jarvisinterface.jsx';
+
+createRoot(document.getElementById('root')).render(<JarvisInterface />);
